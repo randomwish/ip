cacee7b63a4ec03350c24b995fdd7e510bac0d4b
randomwish <13531318+randomwish@users.noreply.github.com>
2026-08-14 02:31:27 +0800

    added marking and unmarking capabilities

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Bro {
     static ArrayList<Task> userTasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String banner = "  ____                \n"
                 + " | __ )  _ __   ___   \n"
@@ -15,17 +16,34 @@ public class Bro {
         System.out.println("Hello, I'm Bro! What drink do you want?");
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
+            String actionWord = userInput.trim().split("\\s+", 2)[0];
+            if (actionWord.equals("list")) {
                 for (int i = 0; i < userTasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + userTasks.get(i));
+                    System.out.println((i + 1) + ". " + "[" + userTasks.get(i).showDone() + "] " + userTasks.get(i).description);
                 }
+            }
+            else if (actionWord.equals("mark")) {
+                // can take it that we are marking an index
+                int index = Integer.parseInt(userInput.trim().split("\\s+", 2)[1]);
+                Task chosenTask = userTasks.get(index-1);
+                chosenTask.isDone = true;
+                System.out.println("Ok this item is marked!");
+                System.out.println("[" + chosenTask.showDone() + "] " + chosenTask.description);
+
+            }
+            else if (actionWord.equals("unmark")) {
+                int index = Integer.parseInt(userInput.trim().split("\\s+", 2)[1]);
+                Task chosenTask = userTasks.get(index - 1);
+                chosenTask.isDone = false;
+                System.out.println("Ok this item is not marked! \n");
+                System.out.println("[" + chosenTask.showDone() + "] " + chosenTask.description);
             } else {
-                Task t = new Task(userInput);
-                userTasks.add(t);
-                System.out.println(t.description);
+                Task newTask = new Task(userInput);
+                userTasks.add(newTask);
+                System.out.println(newTask.description);
             }
             userInput = scanner.nextLine();
         }
-        System.out.println("Ok bye! See you soon.");
+        System.out.println("Goodbye!");
     }
 }
