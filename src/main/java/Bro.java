00c296112e8920a4232a7cf778b21ed4294ed14f
randomwish <13531318+randomwish@users.noreply.github.com>
2026-08-14 01:56:19 +0800

    add and list

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bro {
+    static ArrayList<String> userTasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String banner = "  ____                \n"
                 + " | __ )  _ __   ___   \n"
@@ -13,7 +16,14 @@ public class Bro {
         System.out.println("Hello, I'm Bro! What drink do you want?");
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                for (int i = 0; i < userTasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + userTasks.get(i));
+                }
+            } else {
+                userTasks.add(userInput);
+                System.out.println(userInput);
+            }
             userInput = scanner.nextLine();
         }
         System.out.println("Ok bye! See you soon.");
