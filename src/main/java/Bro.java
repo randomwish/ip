f1953250e6fbffd9c49bd9564215a7c4917f8775
randomwish <13531318+randomwish@users.noreply.github.com>
2026-08-14 01:45:22 +0800

    include echoing

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bro {
     public static void main(String[] args) {
         String banner = "  ____                \n"
@@ -5,9 +7,15 @@ public class Bro {
                 + " |  _ \\ | '__| / _ \\ \n"
                 + " | |_) || |   | (_) | \n"
                 + " |____/ |_|    \\___/  \n";
+        Scanner scanner = new Scanner(System.in);
         System.out.println(banner);
 
-        System.out.println("Hello, I'm Bro! Do you want Milochino?");
+        System.out.println("Hello, I'm Bro! What drink do you want?");
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = scanner.nextLine();
+        }
         System.out.println("Ok bye! See you soon.");
     }
 }
