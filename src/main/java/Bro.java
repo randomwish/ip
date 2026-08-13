95e7b1959b953bf05733d35d00afca6b899a6aec
randomwish <13531318+randomwish@users.noreply.github.com>
2026-08-14 01:32:34 +0800

    Change name from Duke to Bro

---
+++ src/main/java/Bro.java
@@ -0,0 +1,13 @@
+public class Bro {
+    public static void main(String[] args) {
+        String banner = "  ____                \n"
+                + " | __ )  _ __   ___   \n"
+                + " |  _ \\ | '__| / _ \\ \n"
+                + " | |_) || |   | (_) | \n"
+                + " |____/ |_|    \\___/  \n";
+        System.out.println(banner);
+
+        System.out.println("Hello, I'm Bro! Do you want Milochino?");
+        System.out.println("Ok bye! See you soon.");
+    }
+}
