4dd596daa618b13a7a6afd6abbb39df4d65d35c0
randomwish <13531318+randomwish@users.noreply.github.com>
2026-08-15 11:31:13 +0800

    level 4 - todo, events and deadlines

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bro {
@@ -37,7 +38,43 @@ public class Bro {
                 chosenTask.isDone = false;
                 System.out.println("Ok this item is not marked! \n");
                 System.out.println("[" + chosenTask.showDone() + "] " + chosenTask.description);
-            } else {
+            }
+            else if (actionWord.equals("todo")) {
+                String rest = userInput.trim().split("\\s+", 2)[1];
+                ToDos newToDo = new ToDos(rest.trim());
+                userTasks.add(newToDo);
+                System.out.println("Got it. I've added: \n");
+                System.out.println(newToDo.toString());
+                System.out.println("Now you have" + userTasks.size() + " tasks in the list");
+            }
+            else if (actionWord.equals("deadline")) {
+                String rest = userInput.trim().split("\\s+", 2)[1];
+                String[] parts = rest.split("\\s*/by\\s*", 2);
+                String taskDescription = parts[0].trim();
+                String timeDescription = parts[1].trim();
+                Deadlines newDeadline = new Deadlines(timeDescription, taskDescription);
+                userTasks.add(newDeadline);
+
+                System.out.println("Got it. I've added: \n");
+                System.out.println(newDeadline.toString());
+                System.out.println("Now you have " + userTasks.size() + " tasks in the list");
+            }
+
+            else if (actionWord.equals("event")) {
+                String rest = userInput.trim().split("\\s+", 2)[1];
+                String[] fromSplit = rest.split("\\s*/from\\s*", 2);
+                String taskDescription = fromSplit[0].trim();
+                String[] toSplit = fromSplit[1].split("\\s*/to\\s*", 2);
+                String fromTimeDescription = toSplit[0].trim();
+                String toTimeDescription = toSplit[1].trim();
+                Events newEvent = new Events(fromTimeDescription, toTimeDescription, taskDescription);
+                userTasks.add(newEvent);
+                System.out.println("Got it. I've added: \n");
+                System.out.println(newEvent.toString());
+                System.out.println("Now you have " + userTasks.size() + " tasks in the list");
+
+            }
+            else {
                 Task newTask = new Task(userInput);
                 userTasks.add(newTask);
                 System.out.println(newTask.description);
